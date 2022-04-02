e9b2852e384bdd8d8f5e785e6a0526dda88ac04c
Samson Avakyan <avakyansamsonsetraki@gmail.com>
2022-04-03 00:25:53 +0300

    Initialization project

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+
+ReactDOM.render(
+  <Provider store={}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+);
